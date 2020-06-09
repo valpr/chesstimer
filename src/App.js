86c96c73be5d8054acb4e5e67c502799bc9e218e
valpr <me@andrewfong.ca>
2020-06-09 17:49:03 -0400

    Semantic UI CDN insert

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import React from 'react';
 import { Button } from 'semantic-ui-react'
+import { Divider, Grid, Image, Segment } from 'semantic-ui-react'
 
-function App() {
-  return (
-    <div className="App">
-      <Button>
-        10:00 
-        10:00
-      </Button>
-    </div>
-  );
-}
+const App = () => (
+  <Segment>
+    <Grid columns={2} relaxed='very'>
+      <Grid.Column>
+        <p>
+        Lorem Ipsum        </p>
+        <p>
+        Lorem Ipsum        </p>
+        <p>
+        Lorem Ipsum        </p>
+        <p>
+        Lorem Ipsum        </p>
+      </Grid.Column>
+      <Grid.Column>
+        <p>
+          Lorem Ipsum
+        </p>
+        <p>
+        Lorem Ipsum
+        </p>
+        <p>
+        Lorem Ipsum        </p>
+        <p>
+        Lorem Ipsum        </p>
+      </Grid.Column>
+    </Grid>
+
+    <Divider vertical>And</Divider>
+  </Segment>
+)
+
+
+
 
 export default App;
